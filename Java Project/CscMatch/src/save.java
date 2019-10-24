416f41b74b99631c1e972525b806401a306d9452
Austin Chopra <archopra100@icloud.com>
2019-10-23 22:42:06 -0700

    Created the save and open files

---
+++ Java Project/CscMatch/src/save.java
@@ -0,0 +1,21 @@
+/**
+ * @author Austin Chopra
+ * Date: Oct 23, 2019
+ * Course and Section Number:  CSC205AB, 12189
+ * Program Name:  CSC Match - save.java
+ * Program Description:  saves the database
+ *
+ */
+
+public class save {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
