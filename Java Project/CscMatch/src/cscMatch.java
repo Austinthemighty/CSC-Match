ff928e74a34adfd31e4bb7a141770d923078b5a0
Austin Chopra <archopra100@icloud.com>
2019-10-24 00:14:11 -0700

    added the cscMatch.java class, this is the class with all the main code

---
+++ Java Project/CscMatch/src/cscMatch.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+import java.util.Scanner;
+
+
+/**
+ * @author Austin Chopra
+ * Date: Oct 23, 2019
+ * Course and Section Number:  CSC205AB, 12189
+ * Program Name:  main.java
+ * Program Description:  
+ *
+ */
+
+public class cscMatch {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Path filePath = Paths.get("../cscMatch.csc");
+		
+		
+		file.save(filePath);
+	}
+
+}
