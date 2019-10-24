a894885998389a0e1035e777e2357e498a2867c1
Austin Chopra <archopra100@icloud.com>
2019-10-24 00:10:56 -0700

    Created the file Class, this has the open and save functions

---
+++ Java Project/CscMatch/src/file.java
@@ -0,0 +1,41 @@
+/**
+ * @author Austin Chopra
+ * Date: Oct 23, 2019
+ * Course and Section Number:  CSC205AB, 12189
+ * Program Name:  file.java
+ * Program Description: how save and open work:
+ * 							save:
+ * 								save file in the xml format, test file can be found in the root of the cscMatch java folder, take linked list, parse the data and append it to the file
+ * 
+ * 							open:
+ *								opens the file and scrubs the file and saves it to a a linked list.
+ */
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class file {
+
+	/**
+	 * 
+	 */
+	public file() {
+		Path path = null;
+		
+	}
+	
+	public static void save(Path path) {
+		
+	}
+	
+	public static void open(Path path) {
+		
+	}
+	
+}
